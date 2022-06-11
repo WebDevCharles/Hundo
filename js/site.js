379814142b14aab30df412cf9aad3a547f068d11
Charles Hall <webdevcharles@gmail.com>
2022-06-10 21:29:12 -0400

    Code Update

--- js/site.js
+++ js/site.js
@@ -9,23 +9,31 @@ function getValues() {
 function displayValues(start, end) {
 
     element = document.getElementById("results");
-    for (let i = start; i <= end; i++) {
 
-        let item = document.createElement("div");
+    let startNum = parseInt(start);
+    let endNum = parseInt(end);
 
-        if (i % 2 == 0) {
-            item.classList.add("boldItem");
-        } else {
-            item.classList.add("redItem");
+    clrResults();
+
+    element = document.getElementById("results");
+    
+
+        for (let i = startNum; i <= endNum; i++) {
+
+            let item = document.createElement("div");
+
+            if (i % 2 == 0) {
+                item.classList.add("boldItem");
+            } else {
+                item.classList.add("redItem");
+            }
+
+            item.classList.add("col");
+            item.innerHTML = i;
+            element.appendChild(item);
         }
-
-        item.classList.add("col");
-        item.innerHTML = i;
-        element.appendChild(item);
-    }
 }
 
-function clearOutput() {
-    let output = document.getElementById("results");
-    output.innerHTML = "";
+function clrResults() {
+    let output = document.getElementById("results").innerHTML = '';
 }
